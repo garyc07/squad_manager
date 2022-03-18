4becb82d8387a16f677e5efdd910416c649b9e2f
Gary Connolly <garyconnolly07@gmail.com>
2022-03-18 05:03:01 +0000

    2nd Initial Commit :)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-var corsOptions = { origin: "http://localhost:3000" };
+const corsOptions = { origin: "http://localhost:3000" };
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json())
@@ -19,7 +19,6 @@ require('./app/api/routes')(app)
 // Last step error handling, for any unhandled errors that escape
 app.use((err, req, res, next) => {
   console.log(err)
-  //if(err.errorMsg) return res.status(err.statusCode || 500).send({error_msg:err.errorMsg})
   if(err.name === 'UnauthorizedError') return res.status(err.status || 401).send({message:err.message})
   return res.sendStatus(500)
 })
